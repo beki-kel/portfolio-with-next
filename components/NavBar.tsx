d14e1b05b0a72794b43a1efcc9a0ff212e6c698d
beki-kel <139013988+beki-kel@users.noreply.github.com>
2024-07-26 23:36:36 +0300

    chore: Update NavBar component styling and remove ModeToggle component

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { ModeToggle } from '@/components/ModeToggle';
 import  Link  from 'next/link';
 import {FaLinkedin,FaGithub,FaInstagram}  from 'react-icons/fa'
 
@@ -16,23 +15,20 @@ const NavBar = () => {
         { name: 'LinkedIn', url: 'https://www.linkedin.com/in/bereket-kelay/', icon: FaLinkedin },
     ]
 return (
-    <nav className='w-full flex p-3'>
-        <p className='text-3xl w-1/3 font-semibold'> Bereket Kelay 👨‍💻</p>
-        <div className=' ml-auto flex w-1/3 justify-end items-center gap-5'>
+    <nav className='w-full flex p-5 bg-black text-white fixed z-30'>
+        <h1 className='text-3xl w-1/3 font-semibold underline underline-offset-8 decoration-blue-600 -rotate-2'> Bereket Kelay 👨‍💻</h1>
+        <div className=' ml-auto flex w-1/3 justify-end items-center gap-5 pr-10'>
         {
             socials.map((social,index) => 
                 (<div key={index}>
                     <Link href={social.url} target='_blank'>
-                    <div className=' text-4xl w-1/3'>
+                    <div className=' text-4xl w-1/3 hover:scale-125 transition-all'>
                         <social.icon/>
                     </div>
                     </Link>
                 </div>)
             )
         }
-        </div>
-        <div className='pl-24'>
-            <ModeToggle/>
         </div>
     </nav>
 )
