88e2f3350a22eaee3c8c3bd97e61dfc78fd276d7
beki-kel <139013988+beki-kel@users.noreply.github.com>
2024-07-27 16:09:27 +0300

    refactor: Update Home page layout and components, and add Skill component

--- app/(Home)/page.tsx
+++ app/(Home)/page.tsx
@@ -2,18 +2,16 @@ import React from 'react'
 import NavBar from '@/components/NavBar'
 import Hero from './components/Hero'
 import About from "./components/About"
-
-
+import Skill from './components/Skill'
 
 const page = () => {
 
   return (
     <div className='min-h-screen flex flex-col'>
-      <div className='w-full'>
       <NavBar/>
-      </div>
       <Hero/>
       <About/>
+      <Skill/>
     </div>
   )
 }
