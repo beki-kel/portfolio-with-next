fe0b9bc183363028eb7ff04f6901250be387c0c5
beki-kel <139013988+beki-kel@users.noreply.github.com>
2024-07-27 13:34:54 +0300

    feat: Update About component to display education and certificates

--- app/(Home)/components/About.tsx
+++ app/(Home)/components/About.tsx
@@ -1,50 +1,46 @@
+"use client"
 import React from 'react'
 import { GlareCard } from "../../../components/ui/glare-card";
 import { FaSchool } from 'react-icons/fa6';
 import { SiCisco, SiCoursera } from 'react-icons/si';
 import { Button } from "@/components/ui/button"
-
+import Link from 'next/link';
+import { DosisText } from '../../../components/DosisText';
+import { LampContainer } from "../../../components/ui/lamp";
 
 const About = () => {
 return (
-    <div className='min-h-screen w-full bg-slate-950 text-white'>
-        <h1 className='w-full text-center pt-12 text-3xl  font-semibold'> Education And Cerificate</h1>
-        <div className='w-full flex flex-wrap justify-center gap-5 mt-12 '>
-            
-            <GlareCard className="flex flex-col  justify-center items-center ">
-                <FaSchool className='text-7xl'/>
-                <p className="text-white font-semibold text-xl mt-4">
-                    High School Diploma
-                </p>
-                <p className=' text-center text-gray-500'> Sanit Joseph Catholic School,Adama</p>
-                <Button className='bg-black shadow-sm shadow-gray-400 mt-16'> Certificate</Button>
-            </GlareCard>
-
-            <GlareCard className="flex flex-col px-4  items-center justify-center">
+    <div className='min-h-screen pb-24 w-full bg-slate-950 text-white'>
+        <LampContainer >
+            <h1 className='w-full text-center pt-12 text-3xl  font-semibold'> <span className={DosisText.className}>Education And Cerificate</span> </h1>
+            <h3 className='w-full text-center text-xl px-32 pt-4'>Currently I am 4th year Bsc. student in Software Engineering  at Addis Ababa University, Ethiopia. <br/> Here are major certifications I have Accomplished</h3>
+        </LampContainer>
+        <div className='w-full flex flex-wrap justify-center gap-5 -mt-56 '>
+            <GlareCard value="https://coursera.org/verify/9QTMEDS6MHRR" className="flex flex-col px-4  items-center justify-center">
                 <SiCoursera className='text-7xl '/>
                 <p className="text-white font-semibold text-xl text-center mt-4">
                     Developing Front-End Apps with React
                 </p>
                 <p className=' text-center text-gray-500'> Coursera</p>
-                <Button className='bg-black shadow-sm shadow-gray-400 mt-16'> Certificate</Button>
+                <p className='cursor-pointer text-xl underline decoration-white underline-offset-4 focus:text-blue-700  mt-20'> Certificate</p>
             </GlareCard>
 
-            <GlareCard className="flex flex-col  items-center justify-center">
+            <GlareCard value="https://coursera.org/verify/H4FMHWKRXWZP" className="flex flex-col  items-center justify-center">
                 <SiCoursera className='text-7xl '/>
                 <p className="text-white font-semibold text-xl text-center mt-4">
                     Developing Back-End Apps with Node.js and Express
                 </p>
                 <p className=' text-center text-gray-500'> Coursera</p>
-                <Button className='bg-black shadow-sm shadow-gray-400 mt-16'> Certificate</Button>
+                <p className='cursor-pointer text-xl underline decoration-white underline-offset-4 focus:text-blue-700  mt-20'> Certificate</p>
             </GlareCard>
 
-            <GlareCard className="flex flex-col  items-center justify-center">
+            <GlareCard value="https://www.credly.com/badges/11b0376f-eb9e-4025-a42c-30e75e1f175b/public_url" className="flex flex-col  items-center justify-center">
                 <SiCisco className='text-7xl'/>
                 <p className="text-white font-semibold text-xl text-center mt-4">
                     Netwroking Essentials
                 </p>
                 <p className=' text-center text-gray-500'>Cisco</p>
-                <Button className='bg-black  shadow-sm shadow-gray-400 mt-16 rounded-sm cursor-pointer hover:opacity-70'> Certificate</Button>
+                <p className='cursor-pointer text-xl underline decoration-white underline-offset-4 focus:text-blue-700  mt-20'> Certificate</p>
             </GlareCard>
         </div>
         
