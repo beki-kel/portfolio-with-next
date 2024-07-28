1c8c39341fe71b6f1bfca163536f8f97a8822109
beki-kel <139013988+beki-kel@users.noreply.github.com>
2024-07-28 23:15:10 +0300

    refactor: Add NavBar and Footer components to Home page

--- app/(Home)/page.tsx
+++ app/(Home)/page.tsx
@@ -1,12 +1,14 @@
 'use client';
 
-import React, { useState, useEffect } from 'react'
-import Hero from './components/Hero'
-import About from "./components/About"
-import Skill from './components/Skill'
-import Projects from './components/Projects'
-import Services from './components/Services'
-import Loading from '@/components/Loading'
+import React, { useState, useEffect } from 'react';
+import Hero from './components/Hero';
+import About from "./components/About";
+import Skill from './components/Skill';
+import Projects from './components/Projects';
+import Services from './components/Services';
+import Loading from '@/components/Loading';
+import NavBar from './components/NavBar';
+import Footer from "./components/Footer";
 
 
 const Page  = () => {
@@ -27,11 +29,13 @@ const Page  = () => {
       
       {!loading && (
         <div className='min-h-screen flex flex-col'>
+          <NavBar/>
           <Hero/>
           <About/>
           <Skill/>
           <Projects/>
           <Services/>
+          <Footer/>
         </div> )}
     </>
     
