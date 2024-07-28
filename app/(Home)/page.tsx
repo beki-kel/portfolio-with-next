4ab06fb61e057c78bcabf86e43de9cd4f9c49c93
beki-kel <139013988+beki-kel@users.noreply.github.com>
2024-07-28 23:06:38 +0300

    refactor: Update Home page to include lazy loading for images

--- app/(Home)/page.tsx
+++ app/(Home)/page.tsx
@@ -1,22 +1,42 @@
-import React from 'react'
-import NavBar from './components/NavBar'
+'use client';
+
+import React, { useState, useEffect } from 'react'
 import Hero from './components/Hero'
 import About from "./components/About"
 import Skill from './components/Skill'
 import Projects from './components/Projects'
+import Services from './components/Services'
+import Loading from '@/components/Loading'
 
 
-const page = () => {
+const Page  = () => {
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+        setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+}, []);
 
   return (
-    <div className='min-h-screen flex flex-col'>
-      <NavBar/>
-      <Hero/>
-      <About/>
-      <Skill/>
-      <Projects/>
-    </div>
+    <>
+      {loading && <Loading />}
+      
+      {!loading && (
+        <div className='min-h-screen flex flex-col'>
+          <Hero/>
+          <About/>
+          <Skill/>
+          <Projects/>
+          <Services/>
+        </div> )}
+    </>
+    
+    
   )
 }
 
-export default page
+export default Page
