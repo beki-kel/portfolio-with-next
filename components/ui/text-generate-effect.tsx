047e0db1f7c86931d1ae74cebe6d99cabe4ca7a0
beki-kel <139013988+beki-kel@users.noreply.github.com>
2024-07-28 23:16:09 +0300

    refactor: Update text-generate-effect component to center align text

--- components/ui/text-generate-effect.tsx
+++ components/ui/text-generate-effect.tsx
@@ -53,7 +53,7 @@ const renderWords = () => {
 return (
     <div className={cn("font-bold", className)}>
     <div className="mt-4">
-        <div className=" dark:text-white text-black text-2xl leading-snug tracking-wide">
+        <div className=" dark:text-white text-black text-2xl leading-snug tracking-wide text-center">
         {renderWords()}
         </div>
     </div>
